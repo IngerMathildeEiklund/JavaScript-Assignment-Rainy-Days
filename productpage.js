5267744da34fea4c4c01036dabf767f69acbefc4
Inger Mathilde Eiklund <ingeik06345@stud.noroff.no>
2026-02-14 10:41:01 +0100

    fixed on the product page

--- productpage.js
+++ productpage.js
@@ -26,9 +26,18 @@ function displayOneProduct() {
   const productContainer = document.getElementById("product-container");
   const imageContainer = document.getElementById("product-image-container");
   const productInfo = document.getElementById("product-info-container");
+  const sizeAndBTNcontainer = document.getElementById(
+    "size-dropdown-and-button-container",
+  );
   productContainer.classList.add("flex-row");
+  sizeAndBTNcontainer.classList.add("size-dropdown-and-button-container");
+  productInfo.classList.add("margin-left");
 
-  console.log("one product", oneProduct);
+  if (oneProduct.favorite === true) {
+    const favoriteProduct = document.createElement("p");
+    favoriteProduct.textContent = `Best seller!`;
+    productInfo.appendChild(favoriteProduct);
+  }
   const productImage = document.createElement("img");
   const productName = document.createElement("h3");
   const productDesc = document.createElement("p");
@@ -37,7 +46,7 @@ function displayOneProduct() {
   const productColor = document.createElement("p");
   const addToCartBTN = document.createElement("button");
   addToCartBTN.textContent = "Add to cart";
-  addToCartBTN.classList.add("BTN");
+  addToCartBTN.classList.add("btn");
   productImage.src = oneProduct.image.url;
   productImage.alt = oneProduct.title;
   productName.textContent = oneProduct.title;
@@ -45,7 +54,26 @@ function displayOneProduct() {
   productPrice.textContent = oneProduct.price;
   productGender.textContent = `Fit: ${oneProduct.gender}`;
   productColor.textContent = `Color: ${oneProduct.baseColor}`;
+  const sizeDropdown = document.createElement("select");
 
+  const sizePlaceholder = document.createElement("option");
+  sizePlaceholder.textContent = "Select a size";
+  sizePlaceholder.value = "";
+  sizePlaceholder.disabled = true;
+  sizePlaceholder.selected = true;
+  sizeDropdown.appendChild(sizePlaceholder);
+  //add CSS to the size dropdown
+
+  oneProduct.sizes.forEach((size) => {
+    const option = document.createElement("option");
+    option.textContent = size;
+    option.value = size;
+    sizeDropdown.appendChild(option);
+  });
+
+  imageContainer.appendChild(productImage);
+  productInfo.appendChild(productName);
+  productInfo.appendChild(productDesc);
   if (oneProduct.onSale === true) {
     productPrice.classList.add("strike");
     const salePrice = document.createElement("p");
@@ -53,13 +81,12 @@ function displayOneProduct() {
     salePrice.textContent = `On sale! Now ${oneProduct.discountedPrice}`;
     productInfo.appendChild(salePrice);
   }
-  imageContainer.appendChild(productImage);
-  productInfo.appendChild(productName);
-  productInfo.appendChild(productDesc);
   productInfo.appendChild(productPrice);
   productInfo.appendChild(productGender);
   productInfo.appendChild(productColor);
-  productInfo.appendChild(addToCartBTN);
+  sizeAndBTNcontainer.appendChild(sizeDropdown);
+  sizeAndBTNcontainer.appendChild(addToCartBTN);
+  productInfo.appendChild(sizeAndBTNcontainer);
 }
 
 getOneProduct();
